246acb873e492d90f46df5610242e9ebbcdf7240
Yordy <yoalmonte.dev@outlook.com>
2023-08-23 02:09:23 -0400

    base component style complete

--- projects/01-product-card/main.js
+++ projects/01-product-card/main.js
@@ -1,11 +1,8 @@
 import './style.css'
-import javascriptLogo from '/javascript.svg'
-import viteLogo from '/vite.svg'
-import { something } from './components/Product-Card.js'
+import './components/Product-Card.js'
 
 document.querySelector('#app').innerHTML = `
   <product-card>
   </product-card>
 `
 
-something()
